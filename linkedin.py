6481b1bdac3692c42255b8413f8e1271b86e3ce4
Amit Bhargav <72143067+amitbhargav0408@users.noreply.github.com>
2022-09-08 21:51:12 +0530

    Add files via upload

---
+++ linkedin.py
@@ -0,0 +1,13 @@
+#creating a node value 
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.head=None
+
+class linkedlist:
+    def  __init__(self):
+        self.head=None
+
+
+    #insertatbeggining 
+
